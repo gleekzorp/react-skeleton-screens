39196c718bad9fbe3d56daf2fcd156e4c0020e0f
Daniel Floyd <gleek.dan@gmail.com>
2020-10-24 20:14:49 -0600

    added fetch call to test it all out

--- src/components/articles.js
+++ src/components/articles.js
@@ -1,12 +1,31 @@
-const { default: SkeletonArticle } = require("../skeletons/skeleton-article")
+import { useEffect, useState } from "react";
 
-const Articles = (props) => {
+const { default: SkeletonArticle } = require("../skeletons/skeleton-article");
+
+const Articles = () => {
+  const [articles, setArticles] = useState(null);
+
+  // runs automatically after initial render
+  useEffect(() => {
+    setTimeout(async () => {
+      const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+      const data = await res.json();
+      setArticles(data);
+    }, 5000);
+  }, []);
+
   return (
     <div className="articles">
       <h1>Articles</h1>
-      <SkeletonArticle theme="dark"/>
-      <SkeletonArticle />
-      <SkeletonArticle />
+
+      {articles &&
+        articles.map((article) => (
+          <div className="article" key={article.id}>
+            <h3>{article.title}</h3>
+            <p>{article.body}</p>
+          </div>
+        ))}
+      {!articles && [1,2,3,4,5].map((n) => <SkeletonArticle key={n} theme="light" />)}
     </div>
   );
 };
--- src/components/user-details.js
+++ src/components/user-details.js
@@ -1,11 +1,32 @@
+import { useEffect, useState } from "react";
+
 const { default: SkeletonProfile } = require("../skeletons/skeleton-profile");
 
-const UserDetails = (props) => {
+const UserDetails = () => {
+  const [profile, setProfile] = useState(null);
+
+  // runs automatically after initial render
+  useEffect(() => {
+    setTimeout(async () => {
+      const res = await fetch("https://jsonplaceholder.typicode.com/users/1");
+      const data = await res.json();
+      setProfile(data);
+    }, 5000);
+  }, []);
+
   return (
     <div className="user-details">
       <h1>User Details</h1>
-      <SkeletonProfile />
-      <SkeletonProfile theme="dark" />
+
+      {profile && (
+        <div className="profile">
+          <h3>{profile.username}</h3>
+          <p>{profile.email}</p>
+          <a href={profile.website}>{profile.website}</a>
+        </div>
+      )}
+
+      {!profile && <SkeletonProfile />}
     </div>
   );
 };
